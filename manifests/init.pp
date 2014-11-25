78d3a12693e145eca30da5212e14527ab8f9b1fb
Jon Mosco <jonny.mosco@gmail.com>
2014-11-25 14:21:17 -0500

    added comments

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,15 @@
 # == Class: logwatch
 #
+# Parameters
+# $package_ensure
+# $package_name
+#
 class logwatch (
+  $package_ensure = 'installed',
+  $package_name   = 'logwatch',
 ) {
+  package { 'logwatch':
+    ensure => $package_ensure,
+    name   => $package_name,
+  }
 }
